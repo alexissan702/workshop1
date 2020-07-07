0175ca539627509cfe00584bd424b83cc4a91f80
alexissan702 <alexissan702@gmail.com>
2020-07-07 14:24:35 -0700

    added naming.js

---
+++ naming.js
@@ -0,0 +1,14 @@
+var varnum1 = 1
+var varnum2 = 2
+var varnum3 = 3
+var varstr1 = "string1"
+var varstr2 = "string2"
+
+const constnum1 = 1
+const constnum2 = 2
+const constnum3 = 3
+const conststr1 = "constantstring1"
+const conststr2 = "constantstring2"
+
+
+
