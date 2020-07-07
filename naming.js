902d4e1d6df9100321646506418386512b8fc89b
alexissan702 <alexissan702@gmail.com>
2020-07-07 14:59:30 -0700

    added naming.js

--- naming.js
+++ naming.js
@@ -1,19 +1,20 @@
+/*Variables */
 var varnum1 = 1
 var varnum2 = 2
 var varnum3 = 3
 var varstr1 = "string1"
 var varstr2 = "string2"
-
+/*Constants */
 const constnum1 = 1
 const constnum2 = 2
 const constnum3 = 3
 const conststr1 = "constantstring1"
 const conststr2 = "constantstring2"
-
-console.log("varibale 1 ", varnum1);
-console.log("varibale 2 ", varnum2);
-console.log("varibale 3 ", varnum3);
-console.log("varibale 4 ", varstr1);
+/* Console Log */
+console.log("variable 1 ", varnum1);
+console.log("variable 2 ", varnum2);
+console.log("variable 3 ", varnum3);
+console.log("variable 4 ", varstr1);
 console.log("varibale 5 ", varstr1);
 console.log("constant 1 ", constnum1);
 console.log("constant 2 ", constnum2);
