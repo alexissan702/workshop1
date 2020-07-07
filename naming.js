8a398ce6cb9fbb13ebffc39f78d2b0e566765671
alexissan702 <alexissan702@gmail.com>
2020-07-07 14:55:40 -0700

    added naming.js

--- naming.js
+++ naming.js
@@ -10,5 +10,13 @@ const constnum3 = 3
 const conststr1 = "constantstring1"
 const conststr2 = "constantstring2"
 
-
-
+console.log("varibale 1 ", varnum1);
+console.log("varibale 2 ", varnum2);
+console.log("varibale 3 ", varnum3);
+console.log("varibale 4 ", varstr1);
+console.log("varibale 5 ", varstr1);
+console.log("constant 1 ", constnum1);
+console.log("constant 2 ", constnum2);
+console.log("constant 3 ", constnum3);
+console.log("constant 4 ", conststr1);
+console.log("constant 5 ", conststr2);
